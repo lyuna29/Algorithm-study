fe8a370c9b5ce18498f86b5970c1f9ff7717cdc8
lyuna29 <hyunah0418@gmail.com>
2024-10-11 03:08:43 +0900

    알고리즘 보충주석

--- section1/s1-5.js
+++ section1/s1-5.js
@@ -9,6 +9,9 @@ function solution(arr) {
   answer = min;
   return answer;
 }
+// 쉬운 방법 Math.min 가장작은 숫자 찾아주기, Math.max
+// let answer=Math.min(...arr);
+// return answer;
 
 let arr = [5, 7, 1, 3, 2, 9, 11];
 console.log(solution(arr));
--- section1/s1-6.js
+++ section1/s1-6.js
@@ -1,4 +1,18 @@
+//7개의 자연수가 주어질 때 이들 중 홀수인 자연수들을 모두 골라 그 합을 구하고,
+//고른 홀수들 중 최소값을 찾는 프로그램을 작성하시오.
 function solution(arr) {
+  let answer = [];
+  let sum = 0,
+    min = 1000;
+  //or min=Number.Max_SAFE_INTEGER
+  for (let x of arr) {
+    if (x % 2 === 1) {
+      sum += x;
+      if (x < min) min = x;
+    }
+  }
+  answer.push(sum);
+  answer.push(min);
   return answer;
 }
 
