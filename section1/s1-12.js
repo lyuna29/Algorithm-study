3d84f1043f34b384ab46570cd7893861266cf993
lyuna29 <hyunah0418@gmail.com>
2024-11-02 23:55:45 +0900

    charcode

--- section1/s1-12.js
+++ section1/s1-12.js
@@ -7,6 +7,9 @@
 function solution(s) {
   let answer = "";
   for (let x of s) {
+    // let num=x.charCodeAt();
+    //                 if(num>=97 && num<=122) answer+=String.fromCharCode(num-32);
+    //                 else answer+=x;
     if (x === x.toLowerCase()) answer += x.toUpperCase();
     else answer += x;
   }
