122183988fefa19e566fa797f6f921f01fbf461c
lyuna29 <hyunah0418@gmail.com>
2024-12-10 16:28:44 +0900

    section 1-2

---
+++ section-1/s2-2.js
@@ -0,0 +1,16 @@
+//title:삼각형판별하기
+//level:0
+//길이가 서로 다른 A, B, C 세 개의 막대 길이가 주어지면 이 세 막대로 삼각형을 만들 수 있 으면 “YES"를 출력하고,
+//만들 수 없으면 ”NO"를 출력한다.
+function solution(a, b, c) {
+  let answer = "YES";
+  let max = 0;
+  let sum = a + b + c;
+  if (a < b) max = b;
+  else max = c;
+  if (max < c) max = c;
+  if (sum - max <= max) answer = "NO";
+  return answer;
+}
+
+console.log(solution(13, 33, 17));
